ed715a4b2b76d58ec50c9d2c98ed1d5ccc67df6e
ansagang <ansagaang@gmail.com>
2023-09-24 23:01:12 +0600

    tip fix

--- src/components/sections/tip.home.jsx
+++ src/components/sections/tip.home.jsx
@@ -3,25 +3,30 @@ import Image from "next/image"
 export default function Tip({ tip, language }) {
 
     return (
-        <section className="tip">
-            <div className="container__fluid">
-                <div className="tip__inner inner">
-                    {/* <div className="tip__img">
+        tip ?
+            (
+                <section className="tip">
+                    <div className="container__fluid">
+                        <div className="tip__inner inner">
+                            {/* <div className="tip__img">
                         <Image loading='lazy' height={1} width={1} unoptimized={true} title={'logo'} src={'tip-background.svg'} alt={'logo'} />
                     </div> */}
-                    <div className="tip__title title">
-                        <h2>{language.app.pages.main.sections.tip.title}</h2>
-                    </div>
-                    <div className="tip__card">
-                        <div className="tip__card-title title">
-                            <h1>{tip.category.title}</h1>
+                            <div className="tip__title title">
+                                <h2>{language.app.pages.main.sections.tip.title}</h2>
+                            </div>
+                            <div className="tip__card">
+                                <div className="tip__card-title title">
+                                    <h1>{tip.category.title}</h1>
+                                </div>
+                                <div className="tip__card-description info">
+                                    <p>{tip.description}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="tip__card-description info">
-                            <p>{tip.description}</p>
-                        </div>
                     </div>
-                </div>
-            </div>
-        </section>
+                </section>
+            )
+            :
+            null
     )
 }
